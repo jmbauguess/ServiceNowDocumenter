664923d523aeddc2a2849f415ef0e7d89947406c
Justin Bauguess <jmbauguess@gmail.com>
2015-02-12 14:52:38 -0500

    Fixing some dumb syntax errors

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,11 +5,11 @@ from updateSetFetcher import UpdateSetFetcher
 import sys
 
 def main():
-	serviceNow = UpdateSetFetcher()
 	if (len(sys.argv) < 2):
 		print "Invalid use.  Usage: scriptname <update set name> <username> <password> <instance>"
 		sys.exit(2)
-	serviceNow.main(sys.argv[1:])
+	serviceNow = UpdateSetFetcher(sys.argv)
+	serviceNow.main()
 	
 if __name__ == "__main__":
    main()
--- scripts/updateSetFetcher.py
+++ scripts/updateSetFetcher.py
@@ -2,12 +2,16 @@ import requests, json, re, os, sys
 import xml.etree.ElementTree as ET
 
 class UpdateSetFetcher():
-	def __init__(self):
+	def __init__(self, args):
 		print 'Parsing through your Update Set!'
+		self.environment = args[4]
+		self.username = args[2]
+		self.password = args[3]
+		self.updateSet = args[1]
 
-	def main(self, args):
-		print args[1] + " is the Update Set Name"
-		r = requests.get("https://" + args[3] + ".service-now.com/sys_update_xml.do?JSON&sysparm_query=update_set.name=" + args[1], auth=(args[2], self.password[3]));
+	def main(self):
+		print self.updateSet + " is the Update Set Name, Going into " + self.environment + " as " + self.username + ":" + self.password
+		r = requests.get("https://" + self.environment + ".service-now.com/sys_update_xml.do?JSON&sysparm_query=update_set.name=" + self.updateSet, auth=(self.username, self.password));
 		data = r.json()['records']
 		for x in range(0, len(data)):
 			dataType = data[x]['type']
@@ -15,17 +19,18 @@ class UpdateSetFetcher():
 			updater = data[x]['sys_updated_by']
 			updated = data[x]['sys_updated_on']
 			statement =  targetName + " of type " + dataType + " was updated by " + updater + " on " + updated	
+			print statement
 			payload =  data[x]['payload'].encode('utf-8')	
-			tableName = self.determinteTableName(data[x]['name'])
+			tableName = self.determineTableName(data[x]['name'])
 			tree = ET.fromstring(payload)
 			sysIDs = tree.findall("./" + tableName + "//sys_id")
 			for item in sysIDs:
 				print self.getUpdateInformation(item.text, tableName)
 			
 	def getUpdateInformation(self, sysID, tableName):
-		req = requests.get("https://scrippsdev.service-now.com/" + tableName +".do?JSON&sysparm_query=sys_id=" + sysID, auth=(args[2], self.password[3]))
+		req = requests.get("https://" + self.environment + ".service-now.com/" + tableName +".do?JSON&sysparm_query=sys_id=" + sysID, auth=(self.username, self.password))
 		data = req.json()['records']
 		return data
 
-	def determinteTableName(self, name):
+	def determineTableName(self, name):
 		return name[0:name.rindex('_')]	
